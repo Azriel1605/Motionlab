0b5120ee4f32cd38ba54d13d168842d160a510d3
ahmad <ahmad.qeis122@gmail.com>
2024-10-22 12:37:51 +0700

    sadaw

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://qeis:Ra_sy6a7e2@40.82.214.47/motion'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://qeis:Ra_sy6a7e2@20.205.140.207/motion'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
